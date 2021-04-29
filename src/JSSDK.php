d7a8443b4223dae630566cdae7f437bb771d1f4b
xianrenqh <xianrenqh@163.com>
2021-04-29 22:15:16 +0800

    Update JSSDK.php

--- src/JSSDK.php
+++ src/JSSDK.php
@@ -9,12 +9,14 @@
 
 namespace weixin_pay;
 
+use think\Cache;
+
 class ShareH5
 {
 
-    private $appId;
+    public $appId;
 
-    private $appSecret;
+    public $appSecret;
 
     public function __construct($appId, $appSecret)
     {
@@ -24,20 +26,16 @@ class ShareH5
 
     public function getSignPackage()
     {
+        //获取ticket
         $jsapiTicket = $this->getJsApiTicket();
 
-        // 注意 URL 一定要动态获取，不能 hardcode.
-        $protocol = ( ! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url      = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
+        $url       = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
         $timestamp = time();
         $nonceStr  = $this->createNonceStr();
 
         // 这里参数的顺序要按照 key 值 ASCII 码升序排序
-        $string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
-
-        $signature = sha1($string);
-
+        $string      = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+        $signature   = sha1($string);
         $signPackage = array(
             "appId"     => $this->appId,
             "nonceStr"  => $nonceStr,
@@ -63,25 +61,22 @@ class ShareH5
 
     private function getJsApiTicket()
     {
-        // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
-        $jsondata = @file_get_contents("wx_cert/jsapi_ticket.json");
-        $data     = json_decode($jsondata, true);
-        if ( ! empty($data['expire_time']) && ($data['expire_time'] < time())) {
+        // jsapi_ticket 应该全局存储与更新
+        $data = json_decode($this->getCacheTicket());
+        //判断是否过期
+        if ( ! isset($data->expire_time) || $data->expire_time < time()) {
             $accessToken = $this->getAccessToken();
-            // 如果是企业号用以下 URL 获取 ticket
-            // $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
-            $url    = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
-            $res    = json_decode($this->httpGet($url));
-            $ticket = $res->ticket;
+            $url         = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
+            //$res = json_decode($this->https_request($url));
+            $res    = json_decode(file_get_contents($url), 1);
+            $ticket = isset($res['ticket']) ? $res['ticket'] : '';
             if ($ticket) {
-                $data['expire_time']  = time() + 7000;
-                $data['jsapi_ticket'] = $ticket;
-                $fp                   = fopen("/wx_cert/jsapi_ticket.json", "w");
-                fwrite($fp, json_encode($data));
-                fclose($fp);
+                $data->expire_time  = time() + 7000;
+                $data->jsapi_ticket = $ticket;
+                $this->setCacheTicket(json_encode($data));
             }
         } else {
-            $ticket = $data['jsapi_ticket'];
+            $ticket = $data->jsapi_ticket;
         }
 
         return $ticket;
@@ -89,24 +84,20 @@ class ShareH5
 
     private function getAccessToken()
     {
-        // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
-        $jsondata = @file_get_contents("tmp/access_token.json");
-        $data     = json_decode($jsondata, true);
-        if ( ! empty($data['expire_time']) && $data['expire_time'] < time()) {
-            // 如果是企业号用以下URL获取access_token
-            // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
-            $url          = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
-            $res          = json_decode($this->httpGet($url));
-            $access_token = $res->access_token;
+        // access_token 应该全局存储与更新
+        $data = json_decode($this->getCacheToken());
+        if ( ! isset($data->expire_time) || $data->expire_time < time()) {
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
+            //$res = json_decode($this->https_request($url));
+            $res          = json_decode(file_get_contents($url), 1);
+            $access_token = isset($res['access_token']) ? $res['access_token'] : '';
             if ($access_token) {
-                $data['expire_time']  = time() + 7000;
-                $data['access_token'] = $access_token;
-                $fp                   = fopen("wx_cert/access_token.json", "w");
-                fwrite($fp, json_encode($data));
-                fclose($fp);
+                $data->expire_time  = time() + 7000;
+                $data->access_token = $access_token;
+                $this->setCacheToken(json_encode($data));
             }
         } else {
-            $access_token = $data['access_token'];
+            $access_token = $data->access_token;
         }
 
         return $access_token;
@@ -126,4 +117,29 @@ class ShareH5
 
         return $res;
     }
+
+    //获取ticket缓存
+    private function getCacheTicket()
+    {
+        return Cache::has("weixin_share_ticket") ? Cache::get("weixin_share_ticket") : "{}";  //缓存获取，也可自己放入数据库存储
+    }
+
+    //设置ticket缓存
+    private function setCacheTicket($ticket)
+    {
+        return Cache::set("weixin_share_ticket", $ticket, 7000);  //设置缓存
+    }
+
+    //获取token缓存
+    private function getCacheToken()
+    {
+        return Cache::has("weixin_share_token") ? Cache::get("weixin_share_token") : "{}";  //缓存获取，也可自己放入数据库存储
+    }
+
+    //设置token缓存
+    private function setCacheToken($token)
+    {
+        return Cache::set("weixin_share_token", $token, 7000);  //设置缓存
+    }
+
 }
